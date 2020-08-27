aad66134937640b2d6438fed3a3738b08dbcf635
Frederick Gnodtke <frederick@gnodtke.net>
2020-08-27 11:08:15 +0200

    Reproduce loader issue

--- src/index.js
+++ src/index.js
@@ -1,24 +1,17 @@
 import * as PIXI from 'pixi.js';
-import 'regenerator-runtime/runtime'
 
 const app = new PIXI.Application({ backgroundColor: 0x1099bb });
 document.body.appendChild(app.view);
 
-async function main() {
-    const req = await fetch("./TestFont.fnt");
-    console.log(req)
-    const fnt = await req.text();
-    const png = await new Promise(resolve => app.loader.add("./TestFont.png").load(result => {
-        resolve(result.resources["./TestFont.png"]);
-    }))
-    console.log(png)
-    PIXI.BitmapFont.install(fnt, png.texture);
-    const bitmapFontText = new PIXI.BitmapText('012345', { font: 'TestFont', align: 'left' });
+app.loader
+    .add('TestFont', './TestFont.fnt')
+    .load(onAssetsLoaded);
+
+function onAssetsLoaded() {
+    const bitmapFontText = new PIXI.BitmapText('bitmap fonts are supported!\nWoo yay!', { font: 'TestFont', align: 'left' });
 
     bitmapFontText.x = 50;
     bitmapFontText.y = 200;
 
     app.stage.addChild(bitmapFontText);
 }
-
-main();
